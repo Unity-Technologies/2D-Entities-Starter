02979b37475fe32a730f0073ff3f135645643e4b
Paul Tham <paul@unity3d.com>
2020-03-03 16:33:30 +0800

    Initia commit

---
+++ Assets/Tiny2D/Scripts/Runtime/RotationSystem.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace Tiny2D
+{
+    public class RotationSystem : JobComponentSystem 
+    {
+        protected override JobHandle OnUpdate(JobHandle inputDeps)
+        {
+            var deltaTime = Time.DeltaTime;
+            inputDeps = Entities.ForEach((
+                Entity e,
+                ref Rotation rotation,
+                in RotationSpeed rotationSpeed) =>
+            {
+                var rotationAmount = quaternion.RotateZ(rotationSpeed.Value * deltaTime);
+                rotation.Value = math.mul(rotation.Value, rotationAmount);
+                
+            }).Schedule(inputDeps);
+            return inputDeps;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# 2D Entities Starter
+
+------
+
+2D Entities Starter is a template project, which has everything setup for you to get started with the 2D Entities package.
+
+For more information regarding the 2D Entities package, have a look at our package documentation page [LINK PENDING].
+For a list of currently known issues, please have a look at the forum post for [known issues](https://forum.unity.com/threads/first-batch-of-2d-features-for-project-tiny-is-now-available.830652/#post-5513956).
